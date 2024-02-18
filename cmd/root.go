90c10a8945ca26e3dd71516721b2e243bc48a68b
gnarl <nugpah@gmail.com>
2024-02-17 19:54:42 -0500

    refactor startCmd

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -16,7 +15,6 @@ You can also add and remove a timer from the list.`,
 }
 
 func Execute() {
-	fmt.Println("root Execute")
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
@@ -24,15 +22,11 @@ func Execute() {
 }
 
 func init() {
-	fmt.Println("root init")
-
-	// Persistent flags are displayed as global flags in help output.
-	rootCmd.PersistentFlags().StringP("persist", "p", "PersistentDefault", "A persistent flag")
 
 	// Local flag definitions
 	rootCmd.Flags().BoolP("log", "l", false, "A boolean flag with shortcut 'l'")
 	rootCmd.Flags().Bool("mog", false, "A boolean flag without a shortcut")
 
 	// Add subcommands to rootCmd
-	rootCmd.AddCommand(startCmd)
+	rootCmd.AddCommand(NewStartCmd())
 }
--- cmd/start.go
+++ cmd/start.go
@@ -8,35 +8,39 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var startCmd = &cobra.Command{
-	Use:   "start",
-	Short: "Start a new timer",
-	Long:  `Start a new timer. You can specify the duration, name, and action.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		minutes, _ := cmd.Flags().GetInt("duration")
-		seconds := minutes * 60
-		seconds = 5
-		fmt.Print("0 ")
-		for i := 1; i < seconds; i++ {
-			fmt.Print(".")
-			if i%60 == 0 {
-				fmt.Printf("\n%d ", i/60)
-			}
-			time.Sleep(1 * time.Second)
-		}
-		fmt.Println("")
+func NewStartCmd() *cobra.Command {
 
-		message, _ := cmd.Flags().GetString("message")
-		name, _ := cmd.Flags().GetString("name")
-		sendNotification(message, name)
-	},
-}
+	startCmd := &cobra.Command{
+		Use:   "start",
+		Short: "Start a new timer",
+		Long:  `Start a new timer. You can specify the duration, name, and action.`,
+		Run:   runStartCmd,
+	}
 
-func init() {
-	fmt.Println("startCmd init")
+	// Local flag definitions
 	startCmd.Flags().IntP("duration", "d", 35, "The duration the timer runs in minutes")
 	startCmd.Flags().StringP("name", "n", "", "The name of the timer")
 	startCmd.Flags().StringP("message", "m", "Done!", "The action to perform when the timer ends")
+
+	return startCmd
+}
+
+func runStartCmd(cmd *cobra.Command, args []string) {
+	minutes, _ := cmd.Flags().GetInt("duration")
+	seconds := minutes * 60
+	fmt.Print("0 ")
+	for i := 1; i < seconds; i++ {
+		fmt.Print(".")
+		if i%60 == 0 {
+			fmt.Printf("\n%d ", i/60)
+		}
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Println("")
+
+	message, _ := cmd.Flags().GetString("message")
+	name, _ := cmd.Flags().GetString("name")
+	sendNotification(message, name)
 }
 
 func sendNotification(message, name string) {
